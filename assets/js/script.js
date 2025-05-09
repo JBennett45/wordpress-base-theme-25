0ae267e62bb9ca2bd97ceb5aac7a0fba15049b29
Jon Bennett <jon@boxedred.com>
2025-05-09 12:34:27 +0100

    base elements added to allow theme activation basic functions added to functions.php to link in stylesheet and scripts

---
+++ assets/js/script.js
@@ -0,0 +1,1 @@
+// js coming soon //
---
+++ footer.php
@@ -0,0 +1,7 @@
+</main>
+  <footer>
+    <?php get_template_part( 'template-parts/footer/footer-content' ); ?>
+  </footer>
+<?php wp_footer(); ?>
+</body>
+</html>
