5046724114d0ed1c659199b43e946c136d14e935
Jon Bennett <jon@boxedred.com>
2025-05-24 14:01:41 +0100

    Asset Enqueues improved Axios added as a dependency with localized script for theme ajax Example form added in index (temporary) to test axios call Basic Axios and ajax call created in theme and script that logs first and last names with a success - theme ajax is working, the form and script will be removed in next commit

--- functions.php
+++ functions.php
@@ -10,4 +10,21 @@ require_once( 'inc/disable-gutenburg.php' );
 require_once( 'inc/theme-supports.php' );
 //-- [3] Help Functions --//
 require_once( 'functions/helpers.php' );
+// Testing an axios call - usually I'd put axios functions in a file of there own for cleanup, this is just for testing //
+// action name: cst_base_theme_axios_function //
+add_action( 'wp_ajax_nopriv_cst_base_theme_axios_function', 'base_theme_axios_test_call' );
+add_action( 'wp_ajax_cst_base_theme_axios_function', 'base_theme_axios_test_call' );
+function base_theme_axios_test_call() {
+	// // vars //
+	$name = $_POST['first-name'];
+	$lastName = $_POST['last-name'];
+  // you could make queries, store to database and so on here during the axios call //
+  // Send back to axios result //
+ 	echo json_encode( array(
+		'success' => 1,
+    'first-name' => $name,
+    'last-name' => $lastName,
+	));
+	die();
+}
 ?>
