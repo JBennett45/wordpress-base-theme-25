d9e4943a2b46f9b0f2a71cf807b58c9f5e9814ef
Jon Bennett <jon@boxedred.com>
2025-05-28 16:58:32 +0100

    boostrap icons added three tier desktop menu support added, mobile hamburger to follow Readme support for menus editted helper function to include chevron choice

--- functions/helpers.php
+++ functions/helpers.php
@@ -1,15 +1,26 @@
 <?php 
-// [0] Get & Return Menu based off title // 
-function jbfunction_check_menu_exists($menuName, $navWrapper) {
+// [0] Get & Return Menu based off title, optional class for chevron // 
+function jbfunction_check_menu_exists($menuName, $className, $chevron) {
   $menuReturn = wp_get_nav_menu_object($menuName);
+  if(!$className) { $className = 'insert-nav-class-here'; }
+  if(!$chevron) { $chevron = ''; }
   if($menuReturn) {
-    if($navWrapper) { echo '<nav>'; }
-      wp_nav_menu(array(
-        'container' => false,
-        'menu' => __( $menuReturn->name, 'base-theme' ),
-        'menu_class' => 'main-nav'
-      ));
-    if($navWrapper) { echo '</nav>'; }
+    echo '<nav>';
+      if($chevron) {
+        wp_nav_menu(array(
+          'container' => false,
+          'menu' => __( $menuReturn->name, 'base-theme' ),
+          'menu_class' => $className,
+          'link_after' => '<div class="bst-opt-chevron-active-cst">' . file_get_contents( get_template_directory_uri() . '/assets/imgs/bootstrap-icons/chevron-down.svg') . '</div>'
+        ));
+      } else {
+        wp_nav_menu(array(
+          'container' => false,
+          'menu' => __( $menuReturn->name, 'base-theme' ),
+          'menu_class' => $className
+        ));
+      }
+    echo '</nav>';
   }
 }
 // [1] ACF - return active field with tag (h tag etc) //
--- template-parts/header/header-content.php
+++ template-parts/header/header-content.php
@@ -3,6 +3,6 @@
     <span>Logo</span>
   </div>
   <div class="menu-wrapp-cst">
-    <?php jbfunction_check_menu_exists('Header Menu', true); ?>
+    <?php jbfunction_check_menu_exists('Header Menu', 'hdr-optional-class', true); ?>
   </div>
 </div>
