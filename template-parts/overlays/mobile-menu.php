0a4ce2da155ab56fb7e53517bdae6ac18475ec82
Jon Bennett <jon@boxedred.com>
2025-06-16 16:48:44 +0100

    patch header to fixed wrapped overlay in container, moving onto separating header and fundamentals

--- template-parts/overlays/mobile-menu.php
+++ template-parts/overlays/mobile-menu.php
@@ -1,3 +1,5 @@
 <div id="mbl-menu-ovrr-wrapp">
-  <?php jbcst_wp_return_wpmenu('Header Menu', 'hdr-mobile-optional-class', true); ?>
+  <div class="container-cst">
+    <?php jbcst_wp_return_wpmenu('Header Menu', 'hdr-mobile-optional-class', true); ?>
+  </div>
 </div>
