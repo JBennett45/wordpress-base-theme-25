57a606dde78f4ad6421881512b640e44cfd34685
Jon Bennett <jon@boxedred.com>
2025-06-20 11:26:33 +0100

    hamburger tier click events working needed to add support for top level to auto close so not all can be open at the same time but still allows second tier

--- src/js/core/_mobile_toggle_w_menu.js
+++ src/js/core/_mobile_toggle_w_menu.js
@@ -1,4 +1,4 @@
-// mobile overlay toggle //
+// [0] Mobile overlay toggle (open & close) //
 const mobButtonToggle = document.querySelector('.mbl-toggle-wrapp');
 if(mobButtonToggle) {
   mobButtonToggle.addEventListener('click', () => {
@@ -11,3 +11,36 @@ if(mobButtonToggle) {
     }
   });
 }
+// [1] Mobile Chevron dropdown toggle //
+document.querySelectorAll('.mbl-dropdown-watcher-cst .bst-opt-chevron-active-cst').forEach((mobbutton) => {
+  mobbutton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let activeDropdownClass = 'active-mobile-submenu-cst';
+    let activeChevronClass = 'active-mobile-item-cst';
+
+    // control this click event //
+    let closestLi = mobbutton.closest('li');
+    let closestDropdown = closestLi.querySelector('.sub-menu');
+    let closestLiID = closestLi.getAttribute('ID');
+    // // remove existing classes (li) //
+    // document.querySelectorAll('.mbl-dropdown-watcher-cst li').forEach((openDropdown) => {
+    //   let thisMenuItemID = openDropdown.getAttribute('ID');
+    //   // check if its not the current item (toggle controls that) //
+    //   if(thisMenuItemID != closestLiID) {
+    //     let targetSubMenu = document.querySelector('#' + thisMenuItemID + ' .sub-menu');
+    //     // remove li //
+    //     openDropdown.classList.remove(activeChevronClass);
+    //     // remove drodown //
+    //     if(targetSubMenu) {
+    //       targetSubMenu.classList.remove(activeDropdownClass);
+    //     }
+    //   }
+    // });
+    // add class //
+    if(!closestLi.classList.contains(activeDropdownClass)) {
+      closestLi.classList.toggle(activeChevronClass);
+      closestDropdown.classList.toggle(activeDropdownClass);
+    }
+  });
+});
--- template-parts/overlays/mobile-menu.php
+++ template-parts/overlays/mobile-menu.php
@@ -1,6 +1,6 @@
 <div id="mbl-menu-ovrr-wrapp">
   <div class="mbl-menu-ovrr-inner-wrapp">
-    <div class="container-cst">
+    <div class="container-cst mbl-dropdown-watcher-cst">
       <?php jbcst_wp_return_wpmenu('Header Menu', 'hdr-mobile-optional-class', true); ?>
     </div>
   </div>
