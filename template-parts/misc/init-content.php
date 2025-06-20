d98280c5a4ffd30182206c3db595a38ce17868d0
Jon Bennett <jon@boxedred.com>
2025-06-20 11:00:32 +0100

    starter content improved basic content query added if single active

--- template-parts/misc/init-content.php
+++ template-parts/misc/init-content.php
@@ -7,7 +7,14 @@ $fileName = basename($template);
   <div class="container-cst">
     <h1>Welcome to the Base Theme</h1>
     <p>A minimal starter development theme for developers planning to build from the ground up with potential use of ACF</p>
-
-    <h4>Current file name: <?php echo $fileName; ?></h4>
+    <h4>Current file/template: <?php echo $fileName; ?></h4>
+    <?php 
+      if(is_singular('post')) { 
+        while ( have_posts() ) : the_post();
+          echo '<h5>Post example content fetch:</h5>';
+          echo get_the_content();
+        endwhile; 
+      }
+    ?>
   </div>
 </section>
--- template-parts/overlays/mobile-menu.php
+++ template-parts/overlays/mobile-menu.php
@@ -1,5 +1,7 @@
 <div id="mbl-menu-ovrr-wrapp">
-  <div class="container-cst">
-    <?php jbcst_wp_return_wpmenu('Header Menu', 'hdr-mobile-optional-class', true); ?>
+  <div class="mbl-menu-ovrr-inner-wrapp">
+    <div class="container-cst">
+      <?php jbcst_wp_return_wpmenu('Header Menu', 'hdr-mobile-optional-class', true); ?>
+    </div>
   </div>
 </div>
