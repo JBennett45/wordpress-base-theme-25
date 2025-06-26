fcc9c1a57f00646ca53d1fa347fb449eede9fb6a
Jon Bennett <jon@boxedred.com>
2025-06-26 11:21:03 +0100

    Tabbed content is working, need to add the news query and axios

--- src/js/elements/_tabbed_content.js
+++ src/js/elements/_tabbed_content.js
@@ -1,24 +1,24 @@
 // Tab Controller, show and hide relevant content via data-tabid, apply SCSS to the elements directly, basic styles applied to control active and none active states //
-document.querySelectorAll('.tabbed-content-controller-cst .tabbed-control-input-cst').forEach((tabcontrol) => {
+document.querySelectorAll('.jb-tabbed-content-wrapp-cst .tabbed-control-input-cst').forEach((tabcontrol) => {
   tabcontrol.addEventListener('click', (e) => {
-
+    // vars // 
     let activeTabID = tabcontrol.getAttribute('data-tabid').toLowerCase(); 
-    alert(activeTabID);
-
-
+    // Reset active states from tabs and content wrappers //
+    document.querySelectorAll('.jb-tabbed-content-wrapp-cst .tabbed-control-input-cst').forEach((allTabs) => {
+      allTabs.classList.remove('active-tabbed-control-input-cst');
+    });
+    // Active state on clicked item //
+    tabcontrol.classList.add('active-tabbed-control-input-cst');
+    // Content Conrol //
+    document.querySelectorAll('.jb-tabbed-content-wrapp-cst .tabbed-control-output-cst').forEach((allContent) => {
+      // vars // 
+      let targetContent = allContent.getAttribute('data-tabid').toLowerCase(); 
+      // remove all current instances //
+      allContent.classList.remove('active-tabbed-control-output-cst');
+      // add to relevant wrapper //
+      if(activeTabID === targetContent) {
+        allContent.classList.add('active-tabbed-control-output-cst');
+      }
+    });
   });
 });
-
-//  <div class="tabbed-content-controller-cst">
-  //     <div class="tabbed-control-input-cst active-tabbed-control-input-cst" data-tabid="axios">
-  //       Axios
-  //     </div>
-  //     <div class="tabbed-control-input-cst" data-tabid="paginated">
-  //       Paginated
-  //     </div>
-  //   </div>
-  //   <div class="tabbed-content-display-cst">
-  //     <div class="tabbed-control-output-cst active-tabbed-control-output-cst" data-tabid="axios">
-  //       Axios output
-  //     </div>
-  //     <div class="tabbed-control-output-cst" data-tabid="paginated"></div>
--- template-parts/content/post-listing-wrapper.php
+++ template-parts/content/post-listing-wrapper.php
@@ -1,19 +1,21 @@
 <section id="post-selection-mastwrapp-cst">
   <div class="container-cst"> 
-    <div class="tabbed-content-controller-cst">
-      <div class="tabbed-control-input-cst active-tabbed-control-input-cst" data-tabid="axios">
-        Axios
+    <div class="jb-tabbed-content-wrapp-cst">
+      <div class="tabbed-content-controller-cst">
+        <div class="tabbed-control-input-cst active-tabbed-control-input-cst" data-tabid="axios">
+          Axios
+        </div>
+        <div class="tabbed-control-input-cst" data-tabid="paginated">
+          Paginated
+        </div>
       </div>
-      <div class="tabbed-control-input-cst" data-tabid="paginated">
-        Paginated
-      </div>
-    </div>
-    <div class="tabbed-content-display-cst">
-      <div class="tabbed-control-output-cst active-tabbed-control-output-cst" data-tabid="axios">
-        Axios output
-      </div>
-      <div class="tabbed-control-output-cst" data-tabid="paginated">
-        Paginated output
+      <div class="tabbed-content-display-cst">
+        <div class="tabbed-control-output-cst active-tabbed-control-output-cst" data-tabid="axios">
+          Axios output
+        </div>
+        <div class="tabbed-control-output-cst" data-tabid="paginated">
+          Paginated output
+        </div>
       </div>
     </div>
   </div>
