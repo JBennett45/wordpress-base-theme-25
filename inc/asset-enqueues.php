e89ad093f946bf7712b7d9146e69bf4c5db94a34
Jon Bennett <jon@boxedred.com>
2025-10-21 10:21:57 +0100

    swapping webpack out for vite

--- inc/asset-enqueues.php
+++ inc/asset-enqueues.php
@@ -1,19 +1,24 @@
 <?php 
 function jb_cst_theme_styles_and_scripts() {
   if (!is_admin()) {
+    // Bundled assets //
+		$manifestPath = get_theme_file_path('assets/bundle/.vite/manifest.json');
+    if (file_exists($manifestPath)) {
+			$manifest = json_decode(file_get_contents($manifestPath), true);
+			if (isset($manifest['src/js/scripts.js'])) {
+				wp_enqueue_script('mytheme', get_theme_file_uri('assets/bundle/' . $manifest['src/js/scripts.js']['file']));
+				// Enqueue the CSS file
+				wp_enqueue_style('mytheme', get_theme_file_uri('assets/bundle/' . $manifest['src/js/scripts.js']['css'][0]));
+			}
+    }
     // scripts setup //
-    wp_register_script( 'cst-theme-scripts', get_stylesheet_directory_uri() . '/assets/js/scripts.min.js', null, null, true );
 	  wp_register_script( 'cst-theme-ajax', get_stylesheet_directory_uri() . '/assets/js/theme-ajax.js', array( 'jquery' ));
     wp_localize_script( 'cst-theme-ajax', 'theme_ajax', array(
 			'url'		=> admin_url( 'admin-ajax.php' ),
 			'site_url' 	=> get_bloginfo('url'),
 			'theme_url' => get_bloginfo('template_directory')
 		));
-		wp_enqueue_script( 'cst-theme-scripts' );
 		wp_enqueue_script( 'cst-theme-ajax' );
-    // Styles //
-    wp_register_style( 'cst-main-css', get_stylesheet_directory_uri() . '/assets/css/styles.min.css', array(), '', 'all' );
-		wp_enqueue_style( 'cst-main-css' );
   }
 }
 // enqueue base scripts and styles
