a4f4a1cefa0cd51aef5fe556f11aba642c10f7d6
Jon Bennett <jon@boxedred.com>
2025-11-06 15:01:51 +0000

    swiper js support added with function to bring in mulitple instances on one page, all controlled witn attributes mixin clear up colour changed to support custom projects

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,9 @@
 // styles //
 import '../scss/styles.scss'
+import 'swiper/css/bundle';
+// functional/core/dependency //
+import './dependencies/_wpcf_support';
+import './dependencies/swipers/reusable-swiper-attrs';
 // functional/core //
 import "./core/_mobile_toggle_w_menu";
 // elements //
