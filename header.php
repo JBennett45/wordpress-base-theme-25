5fc33b5606c60258128952b7d397c1b14f64f450
Jon Bennett <jon@boxedred.com>
2025-11-13 11:01:51 +0000

    favicon support added

--- header.php
+++ header.php
@@ -7,6 +7,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta property="og:type" content="website">
+  <!-- https://realfavicongenerator.net/ - create package and paste into favicon asset folder -->
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri()?>/assets/imgs/favicon/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="<?php echo get_template_directory_uri()?>/assets/imgs/favicon/favicon.svg" />
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri()?>/assets/imgs/favicon/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri()?>/assets/imgs/favicon/apple-touch-icon.png" />
+  <meta name="apple-mobile-web-app-title" content="Base Theme" />
+  <link rel="manifest" href="<?php echo get_template_directory_uri()?>/assets/imgs/favicon/site.webmanifest" />
+	<meta name="theme-color" content="#FFF" media="(prefers-color-scheme: light)">
+	<meta name="theme-color" content="#000" media="(prefers-color-scheme: dark)">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
