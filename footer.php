4cd9dcd4efff695829d30fb77ec661e0a3b01914
Jon Bennett <jon@boxedred.com>
2026-03-03 21:13:17 +0000

    Footer updates

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 </main>
 <footer id="base-master-fttr-cst" class="basetheme-footer-cst">
-  <div class="container-cst ftr-container-cst">
-    <div class="ftr-container-cst__menu--wrap">
-      <?php jbcst_wp_return_wpmenu('Footer Menu', 'ftr-optional-class', false); ?>
+    <div class="container-cst basetheme-footer-cst__inner" data-contain="yes">
+      <div class="basetheme-footer-cst__menu">
+        <?php jbcst_wp_return_wpmenu('Footer Menu', 'ftr-optional-class', false); ?>
+      </div>
+      <div class="basetheme-footer-cst__terms">
+        <span>&copy; <?php echo date("Y"); ?></span>
+      </div>
     </div>
-    <div class="ftr-container-cst__date--wrap">
-      <span>&copy; <?php echo date("Y"); ?></span>
-    </div>
-  </div>
 </footer>
 <?php get_template_part( 'template-parts/overlays/mobile-menu' );  ?>
 <?php wp_footer(); ?>
